f7424b3995b8188e178233857cbdc2c7b7dd7d84
Kostas Bouzianis <kostasbouzianis@gmail.com>
2026-09-10 12:17:59 +0200

    fixed public/protected routes
    find guest by id is not a public route, the guest can only submit his presence

--- server/routes/guestRoutes.js
+++ server/routes/guestRoutes.js
@@ -10,11 +10,11 @@ import {
 
 const guestRoutes = express.Router();
 
-// the public ones, the guest can submit
+// the public one, the guest can submit
 guestRoutes.post("/add", addGuest);
-guestRoutes.get("/find/:guestId", getGuestById);
 
 // the ones that only the admin can call from the control panel
+guestRoutes.get("/find/:guestId", adminAuth, getGuestById);
 guestRoutes.get("/all", adminAuth, getAllGuests);
 guestRoutes.delete("/delete/:guestId", adminAuth, deleteGuestById);
 guestRoutes.put("/edit/:guestId", adminAuth, updateGuest);
